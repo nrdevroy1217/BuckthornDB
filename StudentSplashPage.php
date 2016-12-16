96b7fbd3d48f6181dca87e4a67779bca86c97fd9
Ben Pfeifer Video <ben@benpfeifervideo.com>
2016-12-15 23:44:23 -0600

    Add Student Access Splash Page
    Added a student access splash page, so students can access reports as
    well as observation creation pages.

---
+++ StudentSplashPage.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset ="utf-8" />
+	<link href="http://www.mathcs.bethel.edu/~nrd83539/Style.css" rel="stylesheet">
+<title>Student Access</title>
+</head>
+<body>
+	<div id="headerPanel">
+    <h1>Buckthorn Database: Student Access</h1>
+    <h4>Please Select from the Following Options:</h4>
+
+	</div>
+	<div id="main">
+		<a href="http://www.mathcs.bethel.edu/~nrd83539/StudentPage.php">Add Observation</a>
+		<a href="http://www.mathcs.bethel.edu/~nrd83539/ReportGenerationPage.php">Generate Reports</a>
+	</div>
+</body>
+</html>
